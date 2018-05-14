0bed2055af08cee1b66224dc5ca6966aa95caa08
Your Name <you@example.com>
2018-05-14 20:03:07 +0200

    hotfix

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,24 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
-import { StyleSheet, Text, View } from 'react-native';
-import Help  from './components/help/help.js'
-import Navigation from './components/navigation/navigation.js';
-import PDFviewer from './components/pdfViewer/pdfViewer.js';
-
-
-
-=======
 import RouterWrapper from './components/routerWrapper/routerWrapper.js';
->>>>>>> origin/master
 
 export default class App extends React.Component {
   state = { fontsAreLoaded: false };
   render() {
-<<<<<<< HEAD
-  return (
-        <PDFviewer />
-      );
-=======
     return (
         <RouterWrapper />
         );
@@ -28,6 +13,5 @@ export default class App extends React.Component {
       'Roboto': require('native-base/Fonts/Roboto.ttf'),
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
     }).then(() => this.setState({fontsAreLoaded: true }));
->>>>>>> origin/master
   }
 }
