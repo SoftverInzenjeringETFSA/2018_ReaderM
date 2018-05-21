1d1f12acba4c1351ed6dd99bd5ad7a50d60b6037
Dino Cosic <dcosic1@etf.unsa.ba>
2018-05-21 17:07:05 +0200

    Frontend za spasavanje na uredjaj

--- Frontend/components/pdfViewerPage/pdfViewerPage.js
+++ Frontend/components/pdfViewerPage/pdfViewerPage.js
@@ -28,16 +28,12 @@ export default class PdfViewerPage extends Component {
 
     saveToDevice = function () {
         console.log("Usao u funkciju spremi na uredjaj!");
-        fetch('http:localhost:5000/savePdfToMobile', {
-            method: 'POST',
+        fetch('http://80.65.65.76:5000/savePdfToMobile?dir=./pdfs/WedMay162018225630GMT0200CentralEuropeanDaylightTime43.pdf', {
+            method: 'GET',
             headers: {
                 Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                firstParam: 'yourValue',
-                secondParam: 'yourOtherValue',
-            }),
+                'Content-Type': 'application/pdfs',
+            }
         });
     }
 
@@ -70,7 +66,7 @@ export default class PdfViewerPage extends Component {
             <View style={{flex: 1, backgroundColor: '#f3f3f3'}}>
                 <ActionButton buttonColor="rgba(231,76,60,1)">
                     <ActionButton.Item buttonColor='#9b59b6' title="Save to device"
-                                       onPress={() => console.log("notes tapped!")}>
+                                       onPress={() => this.saveToDevice()}>
                         <Icon name="md-create" style={styles.actionButtonIcon}/>
                     </ActionButton.Item>
                     <ActionButton.Item buttonColor='#3498db' title="Mark text" onPress={() => {
