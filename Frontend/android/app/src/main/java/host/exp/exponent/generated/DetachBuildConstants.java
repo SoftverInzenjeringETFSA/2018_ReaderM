db7fcccf4c532f08333963f23f875c3a779f6233
Muhamed Delalic <muha.delalic@gmail.com>
2018-05-23 00:53:13 +0200

    android folder izbacen

--- Frontend/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "exp84f9d936e1d94e31a5aadf512732584a://192.168.1.6:19000";
-
-}
